1f45ad6bd5da387e3544361d76ea1d8aad159a44
Samuel Itwaru <43560229+samuelitwaru@users.noreply.github.com>
2020-07-22 16:59:04 +0300

    Add files via upload

---
+++ modules/columns.py
@@ -0,0 +1,26 @@
+class Column:
+	checks = []
+
+
+class String(Column):
+	pass
+
+
+class Integer(Column):
+	pass
+
+
+class Float(Column):
+	pass
+
+
+class Boolean(Column):
+	pass
+
+
+class DateTime(Column):
+	pass
+
+
+class Date(Column):
+	pass
